0262c6f211eaf4620575ab04c3d88b7bce29cae8
Grigory Rechistov <grigory.rechistov@intel.com>
2024-09-30 12:44:31 +0200

    Add failing test case for characterizing original song output

--- cpp/test_song.cpp
+++ cpp/test_song.cpp
@@ -1,1 +1,54 @@
+#include <gmock/gmock.h>
 #include "lib.h"
+
+#include <string>
+using std::string;
+
+class TestSong : public ::testing::Test {};
+
+TEST_F(TestSong, get_song_returns_classic_song) {
+    const string res = get_song();
+
+    const string classic_song("There was an old lady who swallowed a fly.\n" \
+            "I don't know why she swallowed a fly - perhaps she'll die!\n" \
+            "\n" \
+            "There was an old lady who swallowed a spider;\n" \
+            "That wriggled and wiggled and tickled inside her.\n" \
+            "She swallowed the spider to catch the fly;\n" \
+            "I don't know why she swallowed a fly - perhaps she'll die!\n" \
+            "\n" \
+            "There was an old lady who swallowed a bird;\n" \
+            "How absurd to swallow a bird.\n" \
+            "She swallowed the bird to catch the spider,\n" \
+            "She swallowed the spider to catch the fly;\n" \
+            "I don't know why she swallowed a fly - perhaps she'll die!\n" \
+            "\n" \
+            "There was an old lady who swallowed a cat;\n" \
+            "Fancy that to swallow a cat!\n" \
+            "She swallowed the cat to catch the bird,\n" \
+            "She swallowed the bird to catch the spider,\n" \
+            "She swallowed the spider to catch the fly;\n" \
+            "I don't know why she swallowed a fly - perhaps she'll die!\n" \
+            "\n" \
+            "There was an old lady who swallowed a dog;\n" \
+            "What a hog, to swallow a dog!\n" \
+            "She swallowed the dog to catch the cat,\n" \
+            "She swallowed the cat to catch the bird,\n" \
+            "She swallowed the bird to catch the spider,\n" \
+            "She swallowed the spider to catch the fly;\n" \
+            "I don't know why she swallowed a fly - perhaps she'll die!\n" \
+            "\n" \
+            "There was an old lady who swallowed a cow;\n" \
+            "I don't know how she swallowed a cow!\n" \
+            "She swallowed the cow to catch the dog,\n" \
+            "She swallowed the dog to catch the cat,\n" \
+            "She swallowed the cat to catch the bird,\n" \
+            "She swallowed the bird to catch the spider,\n" \
+            "She swallowed the spider to catch the fly;\n" \
+            "I don't know why she swallowed a fly - perhaps she'll die!\n" \
+            "\n" \
+            "There was an old lady who swallowed a horse...\n" \
+            "...She's dead, of course!");
+
+    EXPECT_EQ(res, classic_song);
+}
