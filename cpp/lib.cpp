4a29dcf7ecdb74d6f6a3c0f1e98d349de4bfb509
Grigory Rechistov <grigory.rechistov@intel.com>
2024-09-30 14:53:23 +0200

    Use std::format to form the first line
    Require C++20 for it.

--- cpp/lib.cpp
+++ cpp/lib.cpp
@@ -1,31 +1,34 @@
 #include "lib.h"
 #include <vector>
+#include <format>
+
+const std::string lady_intro = "There was an old lady who swallowed";
 
 const std::string get_verse(size_t n)
 {
     const std::vector<std::string> verses = {
-            "There was an old lady who swallowed a fly.\n" \
+            std::format("{} a fly.\n", lady_intro) +
             "I don't know why she swallowed a fly - perhaps she'll die!\n",
 
-            "There was an old lady who swallowed a spider;\n" \
+            std::format("{} a spider;\n", lady_intro) +
             "That wriggled and wiggled and tickled inside her.\n" \
             "She swallowed the spider to catch the fly;\n" \
             "I don't know why she swallowed a fly - perhaps she'll die!\n",
 
-            "There was an old lady who swallowed a bird;\n" \
+            std::format("{} a bird;\n", lady_intro) +
             "How absurd to swallow a bird.\n" \
             "She swallowed the bird to catch the spider,\n" \
             "She swallowed the spider to catch the fly;\n" \
             "I don't know why she swallowed a fly - perhaps she'll die!\n",
 
-            "There was an old lady who swallowed a cat;\n" \
+            std::format("{} a cat;\n", lady_intro) +
             "Fancy that to swallow a cat!\n" \
             "She swallowed the cat to catch the bird,\n" \
             "She swallowed the bird to catch the spider,\n" \
             "She swallowed the spider to catch the fly;\n" \
             "I don't know why she swallowed a fly - perhaps she'll die!\n",
 
-            "There was an old lady who swallowed a dog;\n" \
+            std::format("{} a dog;\n", lady_intro) +
             "What a hog, to swallow a dog!\n" \
             "She swallowed the dog to catch the cat,\n" \
             "She swallowed the cat to catch the bird,\n" \
@@ -33,7 +36,7 @@ const std::string get_verse(size_t n)
             "She swallowed the spider to catch the fly;\n" \
             "I don't know why she swallowed a fly - perhaps she'll die!\n",
 
-            "There was an old lady who swallowed a cow;\n" \
+            std::format("{} a cow;\n", lady_intro) +
             "I don't know how she swallowed a cow!\n" \
             "She swallowed the cow to catch the dog,\n" \
             "She swallowed the dog to catch the cat,\n" \
@@ -42,7 +45,7 @@ const std::string get_verse(size_t n)
             "She swallowed the spider to catch the fly;\n" \
             "I don't know why she swallowed a fly - perhaps she'll die!\n",
 
-            "There was an old lady who swallowed a horse...\n" \
+            std::format("{} a horse...\n", lady_intro) +
             "...She's dead, of course!"
     };
 
