89bc7ef307421d65b344597213cea3c5bc707e82
Erasmus Cedernaes <erasmus.cedernaes@intel.com>
2024-10-01 15:01:56 +0200

    TDD: X

--- python/song.py
+++ python/song.py
@@ -1,35 +1,35 @@
 def verse(i):
     if i == 0:
         return [
-            "There was an old lady who swallowed a fly.",
-            "I don't know why she swallowed a fly - perhaps she'll die!",
+            "There was an old lady who swallowed a %s." % "fly",
+            "I don't know why she swallowed a %s - perhaps she'll die!" % "fly",
         ]
 
     if i == 1:
         return [
-            "There was an old lady who swallowed a spider;",
+            "There was an old lady who swallowed a %s;" % "spider",
             "That wriggled and wiggled and tickled inside her.",
-            "She swallowed the spider to catch the fly;",
-            "I don't know why she swallowed a fly - perhaps she'll die!"
+            "She swallowed the spider to catch the %s;" % "fly",
+            "I don't know why she swallowed a %s - perhaps she'll die!" % "fly"
         ]
 
     if i == 2:
         return [
-            "There was an old lady who swallowed a bird;",
-            "How absurd to swallow a bird.",
-            "She swallowed the bird to catch the spider,",
-            "She swallowed the spider to catch the fly;",
-            "I don't know why she swallowed a fly - perhaps she'll die!"
+            "There was an old lady who swallowed a %s;" % "bird",
+            "How absurd to swallow a %s." % "bird",
+            "She swallowed the %s to catch the %s," % ("bird", "spider"),
+            "She swallowed the spider to catch the %s;" % "fly",
+            "I don't know why she swallowed a %s - perhaps she'll die!" % "fly"
         ]
 
     if i == 3:
         return [
             "There was an old lady who swallowed a cat;",
-            "Fancy that to swallow a cat!",
-            "She swallowed the cat to catch the bird,",
-            "She swallowed the bird to catch the spider,",
-            "She swallowed the spider to catch the fly;",
-            "I don't know why she swallowed a fly - perhaps she'll die!"
+            "Fancy that to swallow a %s!" % "cat",
+            "She swallowed the %s to catch the %s," % ("cat", "bird"),
+            "She swallowed the %s to catch the %s," % ("bird", "spider"),
+            "She swallowed the %s to catch the %s;" % ("spider", "fly"),
+            "I don't know why she swallowed a fly - perhaps she'll die!",
         ]
 
     if i == 4:
@@ -45,19 +45,19 @@ def verse(i):
 
     if i == 5:
         return [
-            "There was an old lady who swallowed a cow;",
-            "I don't know how she swallowed a cow!",
-            "She swallowed the cow to catch the dog,",
-            "She swallowed the dog to catch the cat,",
-            "She swallowed the cat to catch the bird,",
-            "She swallowed the bird to catch the spider,",
-            "She swallowed the spider to catch the fly;",
-            "I don't know why she swallowed a fly - perhaps she'll die!",
+            "There was an old lady who swallowed a %s;" % "cow",
+            "I don't know how she swallowed a %s!" % "cow",
+            "She swallowed the %s to catch the %s," % ("cow", "dog"),
+            "She swallowed the %s to catch the %s," % ("dog", "cat"),
+            "She swallowed the %s to catch the %s," % ("cat", "bird"),
+            "She swallowed the %s to catch the %s," % ("bird", "spider"),
+            "She swallowed the %s to catch the %s;" % ("spider", "fly"),
+            "I don't know why she swallowed a %s - perhaps she'll die!" % "fly",
         ]
 
     if i == 6:
         return [
-            "There was an old lady who swallowed a horse...",
+            "There was an old lady who swallowed a %s..." % "horse",
             "...She's dead, of course!",
         ]
 
